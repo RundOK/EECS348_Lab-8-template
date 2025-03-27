cf12f276d40006ded2c5f92f0e713869b1ed4429
Rund Al-kofahi <r177a953@ku.edu>
2025-03-27 09:36:28 -0500

    Declaring Functions

--- matrix.cpp
+++ matrix.cpp
@@ -4,6 +4,39 @@
 // function definitions for a class have their names prefixed with
 // the class name and "::"
 
+void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+
+}
+
+
+int Matrix::get_value(std::size_t i, std::size_t j) const {
+    
+}
+
+int Matrix::get_size() const{
+    
+}
+
+int Matrix::sum_diagonal_major() const{
+    
+}
+
+int Matrix::sum_diagonal_minor() const{
+    
+}
+
+void Matrix::swap_rows(std::size_t r1, std::size_t r2){
+    
+}
+
+void Matrix::swap_cols(std::size_t c1, std::size_t c2){
+    
+}
+
+void Matrix::print_matrix() const{
+    
+}
+
 // e.g. for the constructor:
 
 Matrix::Matrix(size_t N) {
